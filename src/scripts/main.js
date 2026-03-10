084b28f5ba960d10040f6dc7e53fd07342798be8
karolinepodlasinski83 <karolinepodlasinski83@gmail.com>
2026-03-10 18:12:11 -0300

    first commit

---
+++ src/scripts/main.js
@@ -0,0 +1,29 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+    const avatar = document.getElementById('avatar');
+    const name = document.getElementById('name');
+    const username = document.getElementById('username');
+    const repos = document.getElementById('repos');
+    const followers = document.getElementById('followers');
+    const following = document.getElementById('following');
+    const link = document.getElementById('link');
+
+    const endpoint = 'https://api.github.com/users/KarolinePodlasinski83';
+
+    fetch(endpoint)
+        .then(response => response.json())
+        .then(data => {
+
+            avatar.src = data.avatar_url;
+            name.innerText = data.name || "Karoline Podlasinski";
+            username.innerText = `@${data.login}`;
+            repos.innerText = data.public_repos;
+            followers.innerText = data.followers;
+            following.innerText = data.following;
+            link.href = data.html_url;
+
+        })
+        .catch(() => {
+            name.innerText = "Erro ao carregar perfil";
+        });
+});
